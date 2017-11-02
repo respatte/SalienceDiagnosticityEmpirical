f8b6bc838f6d60d1f324afdba9edddc9d3251e1b
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2017-11-02 17:04:50 +0000

    Implemented LT.data.import routine function

---
+++ stats/Routines.R
@@ -0,0 +1,9 @@
+# LT DATA IMPORT
+# Function importing looking time data from all participants, in the ../results/ repository by default
+LT.data.import <- function(res.repo="../results/"){
+  df = data.frame()
+  for (file.name in list.files(path=res.repo, pattern=".gazedata")){
+    df <- rbind(df, read.delim(paste0(res.repo,file.name)))
+  }
+  return(df)
+}
