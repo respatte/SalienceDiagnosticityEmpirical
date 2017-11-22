47e0c0594d5380da9399787d079d38f8efd36c50
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2017-11-22 14:28:36 +0000

    Corrected column names

--- stats/GeneralAnalysis.R
+++ stats/GeneralAnalysis.R
@@ -13,8 +13,8 @@ AOIs.adults <- data.frame(name=c("Tail","Head"),L=c(20,400),R=c(220,620),T=c(110
 # Import data and transform into eyetrackingR data
 LT.adults <- LT.data.import() %>% raw.to.ET(AOIs.adults) %>% make_eyetrackingr_data(participant_column = "Subject",
                                                                                     trial_column = "TrialId",
-                                                                                    time_column = "timestamp",
-                                                                                    trackloss_column = "track_loss",
+                                                                                    time_column = "TimeStamp",
+                                                                                    trackloss_column = "TrackLoss",
                                                                                     aoi_columns = c('Head','Tail'),
                                                                                     treat_non_aoi_looks_as_missing = TRUE)
 
