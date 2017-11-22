771c42ca5c063e9c33056845bd18ae9e5ed155d0
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2017-11-22 14:29:12 +0000

    Added CategoryName column to LT df

--- stats/Routines.R
+++ stats/Routines.R
@@ -6,8 +6,6 @@ library(dplyr)
 LT.data.import <- function(res.repo="../results/adults/"){
   single.file.import <- function(file){
     tmp <- read.delim(file)[,-c(2:5,10:23)]
-    tmp$TrialId <- ifelse(tmp$Block==0, tmp$TrialId + 252, tmp$TrialId)
-    tmp$Condition <- factor(ifelse("NoLabelFeedback" %in% tmp$StiLabel,"NoLabel","Label"))
     return(droplevels(tmp[tmp$CurrentObject %in% c("Feedback","Label","Stimulus"),]))
   }
   cl <- makeCluster(4)
@@ -15,6 +13,17 @@ LT.data.import <- function(res.repo="../results/adults/"){
   file.names <- list.files(path=res.repo, pattern=".gazedata")
   df <- foreach(i=1:60,.combine="rbind", .inorder=F) %dopar% single.file.import(paste0(res.repo,file.names[i]))
   stopCluster(cl)
+  df$TrialId <- ifelse(df$Block==0, df$TrialId + 252, df$TrialId)
+  df$Condition <- factor(ifelse("NoLabelFeedback" %in% df$StiLabel,"NoLabel","Label"))
+  df <- df %>% group_by(Subject) %>%
+    mutate(CategoryName = ifelse(any(df$Condition == "NoLabel"),
+                                 "NoName",
+                                 ifelse(any(grepl("A",LT.adults$Stimulus) &
+                                              LT.adults$StiLabel == "Saldie"),
+                                        "A_Saldie",
+                                        "A_Gatoo")
+                                 )
+           )
   df$TrackLoss <- ifelse(pmin.int(df$CursorX,df$CursorY)<0,T,F)
   df$TimeStamp <- df$TimestampMicrosec + df$TimestampSec*1e6
   df <- df[,-(4:5)]
@@ -30,6 +39,6 @@ raw.to.ET <- function(df, AOIs){
     df[,AOI] <- df$CursorX>row$L & df$CursorX<row$R & df$CursorY>row$T & df$CursorY<row$B
   }
   # Set starting time of all trials to 0
-  df <- df %>% group_by(Subject, TrialId) %>% mutate(timestamp = timestamp - min(timestamp))
+  df <- df %>% group_by(Subject, TrialId) %>% mutate(TimeStamp = TimeStamp - min(TimeStamp))
   return(df)
 }
