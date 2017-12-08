54e66476e9d177ad8a136ff6eb7366208a324669
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2017-12-08 18:38:26 +0000

    Debugged up to trackloss_clean

--- stats/InfantsAnalysis.R
+++ stats/InfantsAnalysis.R
@@ -8,8 +8,16 @@ source("Routines.R")
 
 # GATHER DATA
 # Define AOIs
-AOIs.infants.head <- data.frame(AOI_type=c("Reg","Flip"),L=c(1031,1920-1031-450),R=c(1031+450,1920-1031),T=c(197,197),B=c(197+450,197+450))
-AOIs.infants.tail <- data.frame(AOI_type=c("Reg","Flip"),L=c(390,1920-390-450),R=c(390+450,1920-390),T=c(299,299),B=c(299+450,299+450))
+AOIs.infants.head <- data.frame(AOI_type=c("Reg","Flip"),
+                                L=c(1031,1920-1031-450),
+                                R=c(1031+450,1920-1031),
+                                T=c(197,197),
+                                B=c(197+450,197+450))
+AOIs.infants.tail <- data.frame(AOI_type=c("Reg","Flip"),
+                                L=c(390,1920-390-450),
+                                R=c(390+450,1920-390),
+                                T=c(299,299),
+                                B=c(299+450,299+450))
 AOIs.plot <- ggplot(NULL, aes(xmin = L, xmax = R, ymin = T, ymax = B)) +
   xlim(c(0,1920)) + scale_y_reverse(limits = c(1080,0)) +
   geom_rect(data = AOIs.infants.head, aes(fill = AOI_type)) +
@@ -23,11 +31,11 @@ raw_data.infants <- LT_data.infants.import()
 LT.infants <- raw_data.infants %>%
   #add_aoi(., AOIs.infants.head, "CursorX", "CursorY", aoi_name = "Head") %>%
   #add_aoi(., AOIs.infants.tail, "CursorX", "CursorY", aoi_name = "Tail") %>%
-  LT_data.to_eyetrackingR(AOIs.adults, set.trial.start = F) %>%
+  LT_data.to_eyetrackingR(AOIs.adults, participants = "infants") %>%
   make_eyetrackingr_data(participant_column = "ParticipantName",
                          trial_column = "TrialN",
-                         time_column = "RecordingTimestamp",
+                         time_column = "TimeStamp",
                          trackloss_column = "TrackLoss",
                          aoi_columns = c('Head','Tail'),
                          treat_non_aoi_looks_as_missing = F) %>%
-  LT_data.trackloss_clean()
+  LT_data.trackloss_clean(res.repo = "../results/infants/data_cleaning_graphs/")
