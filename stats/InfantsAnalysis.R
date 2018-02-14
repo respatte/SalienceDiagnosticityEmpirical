075ebe614718767e90016e62ce17b325122af3e5
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2018-02-14 11:48:42 +0000

    Infants LT timecourse per block plot

--- stats/InfantsAnalysis.R
+++ stats/InfantsAnalysis.R
@@ -1,5 +1,4 @@
 library(eyetrackingR)
-library(Matrix)
 library(lme4)
 library(tidyverse)
 
@@ -37,3 +36,26 @@ LT.prop_tail_per_block.plot <- ggplot(LT.prop_tail_per_trial,
   geom_violin() +
   geom_boxplot(alpha = 0, outlier.alpha = 1, width = .15,
                position = position_dodge(.9))
+
+# LOOKING TIME ANALYSIS: TIME COURSE ===============================================================
+# Preparing data for analysis and plot
+LT.time_course_aois <- LT.clean %>%
+  make_time_sequence_data(time_bin_size = 50,
+                          aois = c("Head","Tail"),
+                          predictor_columns=c("Condition")) %>%
+  mutate_at("TrialId", as.numeric) %>%
+  subset(TrialId < 25) %>%
+  mutate(Block = ((TrialId-1) %/% 8) + 1)
+# Plotting individual trials
+LT.clean.time_course.plot.blocks <- ggplot(LT.time_course_aois,
+                                           aes(x = Time, y=Prop,
+                                               colour=Condition,
+                                               fill=Condition)) +
+  xlab('Time in Trial') + ylab("Looking to AOI (Prop)") +
+  facet_grid(AOI~Block) + theme(legend.position = "top") + ylim(0,1) +
+  stat_summary(fun.y='mean', geom='line', linetype = '61') +
+  stat_summary(fun.data=mean_se, geom='ribbon', alpha= .25, colour=NA) +
+  geom_hline(yintercept = .5)
+ggsave("../results/infants/LookingTimeCoursePerBlock.pdf",
+       plot = LT.clean.time_course.plot.blocks,
+       width = 6, height = 4)
