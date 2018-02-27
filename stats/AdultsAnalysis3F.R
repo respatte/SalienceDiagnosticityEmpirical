1de799c13b79c97952860c3c3449a5c872564784
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2018-02-27 02:52:52 +0000

    All graphs corrected to APA style

--- stats/AdultsAnalysis3F.R
+++ stats/AdultsAnalysis3F.R
@@ -1,6 +1,7 @@
 library(lme4)
 library(nortest)
 library(tidyverse); library(broom)
+library(jtools)
 library(eyetrackingR)
 
 source("Routines.R")
@@ -37,38 +38,56 @@ LT.time_course_aois <- LT.clean %>%
                                               "Diagnostic")) %>%
   mutate(Part = case_when(Block == 0 ~ "Test",
                           Block == 1 ~ "First Block",
-                          Block == NBlocks ~ "Last Block"))
-# Growth Curve Analysis of the data
-LT.time_course_aois.GCA <- lmer(Prop ~ AOI:(ot1 + ot2 + ot3 + ot4 + ot5 + ot6 + ot7) +
-                                  Condition:AOI:(ot1 + ot2 + ot3 + ot4 + ot5 + ot6 + ot7) +
-                                  (1 | Block) +
-                                  (1 | Stimulus) +
-                                  (1 + ot1 + ot2 + ot3 + ot4 + ot5 + ot6 + ot7 | Participant),
-                                data = LT.time_course_aois, REML = F,
-                                verbose = 2)
-# Plotting eye-tracking data for all AOIs, averaged across trials
-## Plot for first block, last block, and test
-LT.time_course_aois.first_last <- LT.time_course_aois %>%
+                          Block == NBlocks ~ "Last Block")) %>%
   drop_na(Part)
+# # Growth Curve Analysis of the data
+# # Analysing proportions => main effect of Condition or Part nonsensical,
+# # we can only expect differences between AOIs, and between AOIs on different levels
+# LT.time_course_aois.GCA <- lmer(ArcSin ~ (AOI + Condition:AOI + AOI:Part + AOI:Condition:Part)*
+#                                   (ot1 + ot2 + ot3 + ot4 + ot5 + ot6 + ot7) +
+#                                   (1 + AOI + Part + Condition +
+#                                      ot1 + ot2 + ot3 + ot4 + ot5 + ot6 + ot7 | Stimulus) +
+#                                   (1 + AOI + Part + Condition +
+#                                      ot1 + ot2 + ot3 + ot4 + ot5 + ot6 + ot7 | StiLabel) +
+#                                   (1 + AOI + Part +
+#                                      ot1 + ot2 + ot3 + ot4 + ot5 + ot6 + ot7 | Participant),
+#                                 data = LT.time_course_aois, REML = F)
+# ## Check convergence
+# ### Recompute gradient and Hessian with Richardson extrapolation
+# devfun <- update(LT.time_course_aois.GCA, devFunOnly=TRUE)
+# pars <- getME(LT.time_course_aois.GCA, "theta")
+# if(require("numDeriv")){
+#   cat("hess:\n"); print(hess <- hessian(devfun, unlist(pars)))
+#   cat("grad:\n"); print(grad <- grad(devfun, unlist(pars)))
+#   cat("scaled gradient:\n")
+#   print(scgrad <- solve(chol(hess), grad))
+# }
+# print(LT.time_course_aois.GCA@optinfo$derivs)
+# ### Restart the fit from computed values
+# LT.time_course_aois.GCA.restart.1 <- update(LT.time_course_aois.GCA, start = pars)
+# Plotting eye-tracking data and GCA predictions for all AOIs, for first block, last block, and test
 intercept <- tibble(Part = c(rep("First Block", 2), rep("Last Block", 2)),
                     x_int = c(0, 2000, 0, 2000))
 LT.clean.time_course.first_last.plot <- ggplot(LT.time_course_aois.first_last,
                                                aes(x = Time, y=Prop,
                                                    colour=Condition,
                                                    fill=Condition)) +
-  xlab('Time in Trial') + ylab("Looking to AOI (Prop)") +
-  facet_grid(AOI~Part, scales = "free_x") + theme(legend.position = "top") + ylim(0,1) +
+  xlab('Time in Trial') + ylab("Looking to AOI (Prop)") + theme_apa(legend.pos = "top") +
+  scale_colour_discrete(labels = c("Label", "No Label")) +
+  scale_fill_discrete(labels = c("Label", "No Label")) +
+  facet_grid(AOI~Part, scales = "free_x") + ylim(0,1) +
+  scale_x_continuous(breaks = c(-1000, 0, 1000, 2000, 3000)) +
   geom_vline(data = intercept, aes(xintercept = x_int), linetype = "62", alpha = .5) +
   stat_summary(fun.y='mean', geom='line', linetype = '61') +
-  stat_summary(fun.data=mean_se, geom='ribbon', alpha= .25, colour=NA) +
+  stat_summary(fun.data=mean_se, geom='ribbon', alpha= .33, colour=NA) +
   geom_hline(yintercept = 1/3)
 ggsave("../results/adults_3f/LookingTimeCourseFirstLast.pdf",
        plot = LT.clean.time_course.first_last.plot,
-       width = 7, height = 4.3)
+       width = 7, height = 5.4)
 
 # LOOKING TIME ANALYSIS: PROP AOI LOOKING BY PARTICIPANT BY BLOCK ==================================
 # Prepare dataset with BlockTransformation
-LT.prop_aois_per_block <- make_time_window_data(LT.clean,
+LT.prop_aois.per_block <- make_time_window_data(LT.clean,
                                                 aois=c("Tail","Feet","Head"),
                                                 predictor_columns=c("Condition",
                                                                     "Block",
@@ -79,91 +98,43 @@ LT.prop_aois_per_block <- make_time_window_data(LT.clean,
   mutate(OppBlock = Block - NBlocks,
          NormBlock = Block/NBlocks) %>%
   gather("BlockTransformation","Block", Block, OppBlock, NormBlock)
-# Growth Curve Analysis (GCA) for each AOI for each BlockTransformation
-## Set orthogonal polynomials for GCA
-blocks <- LT.prop_aois_per_block %>%
-  {sort(as.vector(unique(.$Block)))}
-orth_poly <- poly(blocks, 7) %>%
-  as.tibble() %>%
-  mutate(Block = blocks)
-colnames(orth_poly) <- c(paste0("ot", 1:7), "Block")
-LT.prop_aois_per_block <- left_join(LT.prop_aois_per_block, orth_poly)
-## Run lmer model for GCA, for each AOI: create subset, run model, drop effects and test sig.
-### Feet
-LT.prop_feet_per_block <- LT.prop_aois_per_block %>%
-  subset(AOI == "Feet")
-LT.prop_feet_per_block.GCA <- lmer(ArcSin ~ Condition * (ot1 + ot2 + ot3 + ot4 + ot5 + ot6 + ot7) +
-                                     (1 | TrialId) +
-                                     (1 | Participant),
-                                   data = LT.prop_feet_per_block, REML = FALSE)
-LT.prop_feet_per_block.comp <- drop1(LT.prop_feet_per_block.GCA, ~., test = "Chi")
-LT.prop_feet_per_block.GCA <- lmer(ArcSin ~ Condition:ot5 +
-                                     (1 | TrialId) +
-                                     (1 | Participant),
-                                   data = LT.prop_feet_per_block, REML = FALSE)
-### Tail
-LT.prop_tail_per_block <- LT.prop_aois_per_block %>%
-  subset(AOI == "Tail")
-LT.prop_tail_per_block.GCA <- lmer(ArcSin ~ Condition * (ot1 + ot2 + ot3 + ot4 + ot5 + ot6 + ot7) +
-                                     (1 | TrialId) +
-                                     (1 | Participant),
-                                   data = LT.prop_tail_per_block, REML = FALSE)
-LT.prop_tail_per_block.comp <- drop1(LT.prop_tail_per_block.GCA, ~., test = "Chi")
-LT.prop_tail_per_block.GCA <- lmer(ArcSin ~ Condition:(ot1 + ot2 + ot7) +
-                                     (1 | TrialId) +
-                                     (1 | Participant),
-                                   data = LT.prop_tail_per_block, REML = FALSE)
-### Head
-LT.prop_head_per_block <- LT.prop_aois_per_block %>%
-  subset(AOI == "Head")
-LT.prop_head_per_block.GCA <- lmer(ArcSin ~ Condition * (ot1 + ot2 + ot3 + ot4 + ot5 + ot6 + ot7) +
-                                     (1 | TrialId) +
-                                     (1 | Participant),
-                                   data = LT.prop_head_per_block, REML = FALSE)
-LT.prop_head_per_block.comp <- drop1(LT.prop_head_per_block.GCA, ~., test = "Chi")
-LT.prop_head_per_block.GCA <- lmer(ArcSin ~ Condition:(ot1 + ot2 + ot7) +
-                                     (1 | TrialId) +
-                                     (1 | Participant),
-                                   data = LT.prop_head_per_block, REML = FALSE)
-# Plot all AOIs and all BlockTransformations
-## Get predicted values from model, add to initial data frame
-LT.prop_feet_per_block$Predicted <- predict(LT.prop_feet_per_block.GCA,
-                                            LT.prop_feet_per_block,
-                                            re.form = NA)
-LT.prop_head_per_block$Predicted <- predict(LT.prop_head_per_block.GCA,
-                                            LT.prop_head_per_block,
-                                            re.form = NA)
-LT.prop_tail_per_block$Predicted <- predict(LT.prop_tail_per_block.GCA,
-                                            LT.prop_tail_per_block,
-                                            re.form = NA)
-LT.prop_aois_per_block <- rbind(LT.prop_feet_per_block,
-                                LT.prop_head_per_block,
-                                LT.prop_tail_per_block)
-## Plot raw data and predicte values from model
-LT.prop_aois_per_block.plot <- ggplot(LT.prop_aois_per_block,
-                                      aes(x = Block, y = ArcSin,
-                                          colour = Condition,
-                                          fill = Condition)) +
-  facet_wrap(~AOI) + theme(aspect.ratio = 1.618/1, legend.position = "top") +
-  #stat_smooth(linetype = "61", level = 0.87) +
-  stat_summary(fun.y = 'mean', geom = 'line', linetype = '61') +
-  stat_summary(fun.data = 'mean_se', geom = 'ribbon', alpha = .25, colour = NA) +
-  stat_summary(aes(y = Predicted), fun.y = 'mean', geom = "line", size = 1.2) +
-  geom_hline(yintercept = asin(sqrt(1/3)))
-ggsave("../results/adults_3f/AOILookingEvolution.png",
-       plot = LT.prop_aois_per_block.plot,
-       width = 7, height = 5)
-# Comparing first block agains last block
-## Plot jitter + mean&se + lines
-LT.prop_aois.first_last <- LT.prop_aois_per_block %>%
+# Comparing first block against last block
+LT.prop_aois.first_last <- LT.prop_aois.per_block %>%
   subset(BlockTransformation == "Block" &
            (Block == 1 | Block == NBlocks)) %>%
   mutate(Part = ifelse(Block == 1, "First Block", "Last Block"))
+## LMER for Prop ~ Condition*Part*AOI
+LT.prop_aois.first_last.lmer.0 <- lmer(ArcSin ~ Part*AOI*Condition - (Part + Condition) +
+                                         (1 + AOI | Participant),
+                                       data = LT.prop_aois.first_last)
+# No main effect of Part since looking at proportions,
+# so overall they should all equate to one when collapsing
+LT.prop_aois.first_last.lmer.1 <- update(LT.prop_aois.first_last.lmer.0,
+                                         . ~ . - Part:AOI:Condition) # Remove
+LT.prop_aois.first_last.lmer.2 <- update(LT.prop_aois.first_last.lmer.1,
+                                         . ~ . - AOI:Condition)
+LT.prop_aois.first_last.lmer.3 <- update(LT.prop_aois.first_last.lmer.2,
+                                         . ~ . - Part:Condition) # Remove
+LT.prop_aois.first_last.lmer.4 <- update(LT.prop_aois.first_last.lmer.3,
+                                         . ~ . - Part:AOI)
+LT.prop_aois.first_last.lmer.5 <- update(LT.prop_aois.first_last.lmer.4,
+                                         . ~ . - AOI)
+LT.prop_aois.first_last.lmer.comp <- anova(LT.prop_aois.first_last.lmer.5,
+                                           LT.prop_aois.first_last.lmer.4,
+                                           LT.prop_aois.first_last.lmer.3,
+                                           LT.prop_aois.first_last.lmer.2,
+                                           LT.prop_aois.first_last.lmer.1,
+                                           LT.prop_aois.first_last.lmer.0)
+LT.prop_aois.first_last.lmer.final <- update(LT.prop_aois.first_last.lmer.0,
+                                             . ~ . - (Part:Condition + Part:AOI:Condition))
+## Plot jitter + lmer mean&se + lines
 LT.prop_aois.first_last.plot <- ggplot(LT.prop_aois.first_last,
                                        aes(x = Part, y = Prop,
                                            colour = Condition,
                                            fill = Condition)) +
-  facet_wrap(~AOI) + theme(aspect.ratio = 1.618/1, legend.position = "top") +
+  facet_wrap(~AOI) + theme_apa(legend.pos = "top") + ylab("Looking to AOI (Prop)") +
+  scale_colour_discrete(labels = c("Label", "No Label")) +
+  scale_fill_discrete(labels = c("Label", "No Label")) +
   geom_point(position = position_jitterdodge(dodge.width = .8,
                                              jitter.width = .2),
              alpha = .25) +
@@ -178,36 +149,7 @@ LT.prop_aois.first_last.plot <- ggplot(LT.prop_aois.first_last,
              position = position_dodge(.1))
 ggsave("../results/adults_3f/AOILookingFirstLast.pdf",
        LT.prop_aois.first_last.plot,
-       width = 9, height = 6)
-## LMER for Prop ~ Condition*Part*AOI
-LT.prop_aois.first_last.lmer.0 <- lmer(ArcSin ~ Part*AOI*Condition - Part +
-                                         (1 + AOI | Participant),
-                                       data = LT.prop_aois.first_last)
-# No main effect of Part since looking at proportions,
-# so overall they should all equate to one when collapsing
-LT.prop_aois.first_last.lmer.1 <- update(LT.prop_aois.first_last.lmer.0,
-                                         . ~ . - Part:AOI:Condition) # Remove
-LT.prop_aois.first_last.lmer.2 <- update(LT.prop_aois.first_last.lmer.1,
-                                         . ~ . - AOI:Condition)
-LT.prop_aois.first_last.lmer.3 <- update(LT.prop_aois.first_last.lmer.2,
-                                         . ~ . - Part:Condition) # Remove
-LT.prop_aois.first_last.lmer.4 <- update(LT.prop_aois.first_last.lmer.3,
-                                         . ~ . - Part:AOI)
-LT.prop_aois.first_last.lmer.5 <- update(LT.prop_aois.first_last.lmer.4,
-                                         . ~ . - Condition) # Remove
-LT.prop_aois.first_last.lmer.6 <- update(LT.prop_aois.first_last.lmer.5,
-                                         . ~ . - AOI)
-LT.prop_aois.first_last.lmer.comp <- anova(LT.prop_aois.first_last.lmer.6,
-                                           LT.prop_aois.first_last.lmer.5,
-                                           LT.prop_aois.first_last.lmer.4,
-                                           LT.prop_aois.first_last.lmer.3,
-                                           LT.prop_aois.first_last.lmer.2,
-                                           LT.prop_aois.first_last.lmer.1,
-                                           LT.prop_aois.first_last.lmer.0)
-LT.prop_aois.first_last.lmer.final <- update(LT.prop_aois.first_last.lmer.0,
-                                             . ~ . - (Condition +
-                                                        Part:Condition +
-                                                        Part:AOI:Condition))
+       width = 7, height = 5.4)
 
 # BEHAVIOURAL ANALYSIS: PARTICIPANTS AND BLOCKS ====================================================
 # Get how many participants for each block, and make a bar plot
@@ -217,20 +159,25 @@ behaviour.parts_per_block <- behaviour %>%
   summarise(N_Participants = n_distinct(Participant))
 behaviour.parts_per_block.plot <- ggplot(behaviour.parts_per_block,
                                          aes(x = Block, y = N_Participants, fill = Condition)) +
+  theme_apa(legend.pos = "topright") + scale_fill_discrete(labels = c("Label", "No Label")) +
+  scale_x_continuous(breaks = 1:10) + ylab("Participants") +
   geom_col(position = "dodge")
-ggsave("../results/adults_3f/ParticipantsPerBlock.png",
-       plot = behaviour.parts_per_block.plot)
+ggsave("../results/adults_3f/ParticipantsPerBlock.pdf", plot = behaviour.parts_per_block.plot,
+       width = 3.5, height = 2.7)
 # Get number of blocks to learning per participant, plot a violin
 behaviour.blocks_per_part <- behaviour %>%
   select(c(Participant, Condition, NBlocks)) %>%
   unique()
 behaviour.blocks_per_part.plot <- ggplot(behaviour.blocks_per_part,
                                          aes(x = Condition, y = NBlocks, fill = Condition)) +
+  theme_apa(legend.pos = "topright") + ylab("Blocks") +
+  scale_fill_discrete(labels = c("Label", "No Label")) +
   geom_violin() +
   geom_boxplot(alpha = 0, outlier.alpha = 1, width = .15)
-ggsave("../results/adults_3f/BlocksPerParticipant.png",
-       plot = behaviour.blocks_per_part.plot)
-# BEHAVIOURAL ANALYSIS: ACCURACY ~ CONDITION*(DIAG || RT) ==========================================
+ggsave("../results/adults_3f/BlocksPerParticipant.pdf", plot = behaviour.blocks_per_part.plot,
+       width = 3.5, height = 2.9)
+
+# BEHAVIOURAL ANALYSIS: ACCURACY ~ CONDITION*DIAG*RT) ==============================================
 # Get datasets for training and test
 behaviour.training <- behaviour %>%
   subset(Phase == "Familiarisation")
@@ -250,26 +197,32 @@ ACC_by_diag.training.glmer <- glmer(ACC ~ Condition*Diagnostic*zLogRT +
 ##                                data = behaviour.test)
 # Prepare and plot data
 ## During training
-ACC_by_diag.training <- behaviour.training %>%
+ACC_by_diag_by_RT.training <- behaviour.training %>%
   group_by(Participant, Diagnostic, Condition) %>%
   summarise(Accuracy = sum(ACC)/n())
-ACC_by_diag.training.plot <- ggplot(ACC_by_diag.training,
+ACC_by_diag_by_RT.training.plot <- ggplot(ACC_by_diag_by_RT.training,
                                     aes(x = Condition,
                                         y = Accuracy,
                                         fill = Diagnostic)) +
-  ylim(0,1) +
+  ylim(0,1) + theme_apa(legend.pos = "bottomright") +
+  scale_x_discrete(labels = c("Label", "No Label")) +
   geom_violin() +
   geom_boxplot(alpha = 0, outlier.alpha = 1,
                width = .15, position = position_dodge(.9))
+ggsave("../results/adults_3f/ACCbyRTbyDiag_training.pdf", plot = ACC_by_diag_by_RT.training.plot,
+       width = 3.5, height = 2.7)
 ## At test
-ACC_by_diag.test <- behaviour.test %>%
+ACC_by_diag_by_RT.test <- behaviour.test %>%
   group_by(Participant, Diagnostic, Condition) %>%
   summarise(Accuracy = sum(ACC)/n())
-ACC_by_diag.test.plot <- ggplot(ACC_by_diag.test,
+ACC_by_diag_by_RT.test.plot <- ggplot(ACC_by_diag_by_RT.test,
                                 aes(x = Condition,
                                     y = Accuracy,
                                     fill = Diagnostic)) +
-  ylim(0,1) +
+  ylim(0,1) + theme_apa(legend.pos = "bottomright") +
+  scale_x_discrete(labels = c("Label", "No Label")) +
   geom_violin() +
   geom_boxplot(alpha = 0, outlier.alpha = 1,
                width = .15, position = position_dodge(.9))
+ggsave("../results/adults_3f/ACCbyRTbyDiag_test.pdf", plot = ACC_by_diag_by_RT.test.plot,
+       width = 3.5, height = 2.7)
