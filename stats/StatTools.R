94f87ea2ff470ff88c6cfd1aa56c45ad41a913c7
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2018-03-23 15:56:19 +0000

    lmer.SS2 formula creation structure

--- stats/StatTools.R
+++ stats/StatTools.R
@@ -6,4 +6,11 @@ library(tidyverse)
 lmer_test.SS2.par <- function(formula, data){
   terms <- tibble(label = attr(terms(f), "term.labels"),
                   order = attr(terms(f), "order"))
+  n.order <- max(terms$order)
+  for(i in 1:n.order){
+    n.order_i <- sum(terms$order == i)
+    for(j in 1:n.order_i){
+      #TODO
+    }
+  }
 }
