92250e11a8336a48cce69ca704cee1a263a898e5
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2018-03-27 11:46:32 +0100

    lmer.SS2 generating intercept formula

--- stats/StatTools.R
+++ stats/StatTools.R
@@ -3,14 +3,20 @@ library(lme4)
 library(doSNOW)
 library(tidyverse)
 
-lmer_test.SS2.par <- function(formula, data){
-  terms <- tibble(label = attr(terms(f), "term.labels"),
-                  order = attr(terms(f), "order"))
+lmer_test.SS2.par <- function(formula, data = NULL){
+  # Generate all formulae
+  terms <- tibble(label = attr(terms(formula), "term.labels"),
+                  order = attr(terms(formula), "order")) %>%
+    subset(!grepl("\\|", label))
   n.order <- max(terms$order)
-  for(i in 1:n.order){
-    n.order_i <- sum(terms$order == i)
-    for(j in 1:n.order_i){
-      #TODO
-    }
-  }
+  formulae <- list("Intercept" = update.formula(formula,
+                                                str_c(". ~ . - ",
+                                                      str_c(terms$label, collapse = " - "))))
+  # for(i in 1:n.order){
+  #   n.order_i <- sum(terms$order == i)
+  #   order_i <- terms %>% subset(order <= i)
+  #   for(j in 1:n.order_i){
+  # 
+  #   }
+  # }
 }
