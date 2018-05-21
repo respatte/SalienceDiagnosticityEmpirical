a0376ef3c72f69062608b2136924bb077e9b8a92
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2018-05-21 13:20:03 +0100

    Changed Fam trial start to fit label onset

--- stats/InfantsAnalysis.R
+++ stats/InfantsAnalysis.R
@@ -7,28 +7,29 @@ source("Routines.R")
 
 # GATHER DATA ======================================================================================
 d <- LT_data.gather("infants")
-LT.clean <- d[[4]] %>%
+LT.fam.clean <- d[[4]] %>%
+  subset(Phase = "Familiarisation") %>%
   make_eyetrackingr_data(participant_column = "Participant",
                          trial_column = "TrialId",
                          time_column = "TimeStamp",
                          trackloss_column = "TrackLoss",
                          aoi_columns = c("Head","Tail"),
                          treat_non_aoi_looks_as_missing = T) %>%
-  subset_by_window(window_start_time = 1500, window_end_time = 7000)
+  subset_by_window(window_start_col = "LabelOnset", window_end_col = "TrialEnd")
 # Check for available PresentationSequence
-pres_seq <- LT.clean %>%
+pres_seq <- LT.fam.clean %>%
   group_by(PresentationSequence, Participant) %>%
   summarise(T = sum(TrackLoss)/n())
-gender <- LT.clean %>%
+gender <- LT.fam.clean %>%
   group_by(Gender, Condition) %>%
   summarise(N = n_distinct(Participant))
-age <- LT.clean %>%
+age <- LT.fam.clean %>%
   group_by(Participant, Condition) %>%
   summarise(Age = first(Age))
 
 # LOOKING TIME ANALYSIS: PROP AOI LOOKING BY PARTICIPANT BY TRIAL/BLOCK ============================
 # Prepare dataset, include only familiarisation
-LT.prop_tail <- make_time_window_data(LT.clean,
+LT.prop_tail <- make_time_window_data(LT.fam.clean,
                                                 aois=c("Tail"),
                                                 predictor_columns=c("Condition",
                                                                     "TrialId",
@@ -82,7 +83,7 @@ ggsave("../results/infants/AOILookingPerParts.pdf",
 
 # LOOKING TIME ANALYSIS: TIME COURSE ===============================================================
 # DATA PREPARATION
-LT.time_course_aois <- LT.clean %>%
+LT.time_course_aois <- LT.fam.clean %>%
   subset(Phase == "Familiarisation") %>%
   make_time_sequence_data(time_bin_size = 50,
                           aois = "Tail",
@@ -105,7 +106,7 @@ LT.time_cluster_aois.analysis <- analyze_time_clusters(LT.time_cluster_aois,
 # PLOT
 intercept <- tibble(Part = 0:2,
                     x_int = rep(1500,3)) # Label onset ish (second half trials includes "the")
-LT.clean.time_course.plot.blocks <- ggplot(LT.time_course_aois,
+LT.fam.clean.time_course.plot.blocks <- ggplot(LT.time_course_aois,
                                            aes(x = Time, y=Prop,
                                                colour=Condition,
                                                fill=Condition)) +
@@ -117,5 +118,5 @@ LT.clean.time_course.plot.blocks <- ggplot(LT.time_course_aois,
   geom_hline(yintercept = .5) +
   geom_vline(data = intercept, aes(xintercept = x_int), linetype = "62", alpha = .5)
 ggsave("../results/infants/LookingTimeCoursePerPart.pdf",
-       plot = LT.clean.time_course.plot.blocks,
+       plot = LT.fam.clean.time_course.plot.blocks,
        width = 7, height = 2.5)
