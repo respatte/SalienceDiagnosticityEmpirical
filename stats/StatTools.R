85274e4321dde309ffaf051de0b59deafe72eab6
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2018-07-03 17:23:46 +0100

    Function to compute nested brm models and associated b-values
    Former-commit-id: d4438aeea5b19834c3be28965e8244683e10fecc

--- stats/StatTools.R
+++ stats/StatTools.R
@@ -1,22 +1,45 @@
 # LIBRARY IMPORTS ==================================================================================
-library(lme4)
-library(doSNOW)
+library(brms)
 library(tidyverse)
 
-lmer_test.SS2.par <- function(formula, data = NULL){
-  # Generate all formulae
-  terms <- tibble(label = attr(terms(formula), "term.labels"),
-                  order = attr(terms(formula), "order")) %>%
-    subset(!grepl("\\|", label))
-  n.order <- max(terms$order)
-  formulae <- list("Intercept" = update.formula(formula,
-                                                str_c(". ~ . - ",
-                                                      str_c(terms$label, collapse = " - "))))
-  # for(i in 1:n.order){
-  #   n.order_i <- sum(terms$order == i)
-  #   order_i <- terms %>% subset(order <= i)
-  #   for(j in 1:n.order_i){
-  # 
-  #   }
-  # }
+# BRM FIXEF BAYES FACTOR 
+# Function computing all nested models in formulas nd computing Bayes factors for pairs of models
+# Formulas must be given with no-intercept formulas first, then intercept-only formulas, and
+# finally other formulas.
+# no_intercept is the number of formulas with no intercept (default = 0)
+# intercept_only is the number of formulas with intercept-only (default = 1)
+# priors is a list with the prior for intercept-only models and priors for other models
+# (no prior for no-intercept models)
+bayes_factor.brm_fixef <- function(formulas, df, priors,
+                                   no_intercept = 0, intercept_only = 1,
+                                   controls = NULL){
+  ## Run all Bayesian models
+  models <- lapply(seq_along(formulas),
+                   function(i){
+                     brm(formulas[[i]],
+                         data = df,
+                         prior = if(i <= no_intercept){
+                           NULL
+                         }else{
+                           if(i <= no_intercept + intercept_only){
+                             # Intercept-only model, no slope priors
+                             priors[[1]]
+                           }else{
+                             # Slope priors
+                             priors[[2]]
+                           }
+                         },
+                         chains = 4, cores = 4,
+                         control = controls,
+                         save_all_pars = T)
+                   })
+  ## Compute all Bayes fators
+  n_models <- length(formulas)
+  bayes_factors <- lapply(2:n_models,
+                          function(i){
+                            bayes_factor(models[[i]],
+                                         models[[i-1]])
+                          })
+  ## Return both lists as a list
+  return(list("models" = models, "BF" = bayes_factors))
 }
